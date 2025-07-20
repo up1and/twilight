0be03bc67529b248489bbb56a96372212a2af341
up1and <piratecb@gmail.com>
2025-07-20 10:58:49 +0800

    refactor: migrate from s3fs to boto3 for S3 operations

--- worker/sync.py
+++ worker/sync.py
@@ -4,14 +4,15 @@ Syncs latest AHI-L1b-FLDK data from NOAA S3 to local MinIO
 """
 import sys
 import time
-import requests
 
-import s3fs
+import boto3
+import botocore
+import requests
 
 from io import BytesIO
 
 from utils import logger
-from client import get_minio_client
+from config import endpoint, access_key, secret_key
 
 # Configuration
 noaa_bucket = 'noaa-himawari9'
@@ -156,52 +157,45 @@ class SyncProcessor:
     """Synchronizes Himawari-9 data from NOAA S3 to local MinIO"""
 
     def __init__(self, sync_client: SyncClient):
-        # Initialize NOAA S3 filesystem (anonymous access)
-        self.noaa_fs = s3fs.S3FileSystem(anon=True)
+        # Initialize NOAA S3 client (anonymous access)
+        self.noaa_s3 = boto3.client(
+            's3',
+            config=botocore.config.Config(signature_version=botocore.UNSIGNED)
+        )
 
-        # Initialize local MinIO client
-        self.client = get_minio_client()
+        # Initialize local MinIO client using boto3
+        self.minio_s3 = boto3.client(
+            's3',
+            endpoint_url=f'http://{endpoint}',
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
+            region_name='us-east-1'  # MinIO requires a region
+        )
 
         self.sync_client = sync_client
 
         # Ensure local bucket exists
-        if not self.client.bucket_exists(local_bucket):
-            self.client.make_bucket(local_bucket)
-
-    def count_local_files(self, time_folder):
-        """Count files in local bucket for given time folder"""
         try:
-            files = self.list_files(self.client, local_bucket, time_folder)
-            return len(files)
-        except Exception as e:
-            logger.error(f"Error counting local files in {time_folder}: {e}")
-            return 0
+            self.minio_s3.head_bucket(Bucket=local_bucket)
+        except:
+            self.minio_s3.create_bucket(Bucket=local_bucket)
 
-    def list_files(self, client, bucket, time_prefix):
+    def list_files(self, s3_client, bucket, time_prefix):
         """List files in a bucket with given prefix and extension"""
         try:
             files = []
-
-            # Check if it's s3fs or minio client
-            if isinstance(client, s3fs.S3FileSystem):
-                # s3fs client
-                path = f"{bucket}/{time_prefix}"
-                try:
-                    file_list = client.ls(path, detail=False)
-                    for file_path in file_list:
-                        if file_path.endswith('.DAT.bz2'):
-                            # Remove bucket name from path to get object key
-                            object_key = file_path.replace(f"{bucket}/", "")
-                            files.append(object_key)
-                except FileNotFoundError:
-                    # Directory doesn't exist
-                    pass
-            else:
-                # minio client
-                objects = client.list_objects(bucket, prefix=time_prefix, recursive=True)
-                for obj in objects:
-                    if obj.object_name.endswith('.DAT.bz2'):
-                        files.append(obj.object_name)
+            paginator = s3_client.get_paginator('list_objects_v2')
+            
+            page_iterator = paginator.paginate(
+                Bucket=bucket,
+                Prefix=time_prefix
+            )
+            
+            for page in page_iterator:
+                if 'Contents' in page:
+                    for obj in page['Contents']:
+                        if obj['Key'].endswith('.DAT.bz2'):
+                            files.append(obj['Key'])
 
             return files
 
@@ -212,10 +206,9 @@ class SyncProcessor:
     def sync_file(self, object_name):
         """Copy file from NOAA S3 to local MinIO with progress tracking"""
         try:
-            # Get file info from s3fs
-            s3_path = f"{noaa_bucket}/{object_name}"
-            file_info = self.noaa_fs.info(s3_path)
-            file_size = file_info['size']
+            # Get file info from NOAA S3
+            response = self.noaa_s3.head_object(Bucket=noaa_bucket, Key=object_name)
+            file_size = response['ContentLength']
 
             filename = object_name.split('/')[-1]
 
@@ -227,10 +220,12 @@ class SyncProcessor:
 
             # Download from NOAA S3 in chunks and write to stream
             chunk_size = 1024 * 1024  # 1MB chunks
-
-            with self.noaa_fs.open(s3_path, 'rb') as f:
+            
+            response = self.noaa_s3.get_object(Bucket=noaa_bucket, Key=object_name)
+            
+            with response['Body'] as body:
                 while True:
-                    chunk = f.read(chunk_size)
+                    chunk = body.read(chunk_size)
                     if not chunk:
                         break
 
@@ -242,13 +237,12 @@ class SyncProcessor:
             # Reset stream position for upload
             stream.seek(0)
 
-            # Upload to local MinIO using streaming
-            self.client.put_object(
-                bucket_name=local_bucket,
-                object_name=object_name,
-                data=stream,
-                length=file_size,
-                part_size=10*1024*1024  # 10MB parts
+            # Upload to local MinIO
+            self.minio_s3.put_object(
+                Bucket=local_bucket,
+                Key=object_name,
+                Body=stream,
+                ContentLength=file_size
             )
 
             stream.close()
@@ -270,12 +264,12 @@ class SyncProcessor:
         status = 'pending'
 
         # List files in NOAA S3
-        noaa_files = self.list_files(self.noaa_fs, noaa_bucket, time_folder)
+        noaa_files = self.list_files(self.noaa_s3, noaa_bucket, time_folder)
         if not noaa_files:
             logger.warning(f"No files found in NOAA S3 for {time_folder}")
             return status
 
-        existing_files = set(self.list_files(self.client, local_bucket, time_folder))
+        existing_files = set(self.list_files(self.minio_s3, local_bucket, time_folder))
         file_count = len(existing_files)
 
         # Find files that need to be synced
